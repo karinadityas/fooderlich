e46073137361dc2089ff13d46e0469f8ed4b4ab3
karinadityas <karina.adityasramadhanti2041@students.unila.ac.id>
2022-09-22 13:57:33 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'home.dart';
+import 'myInheritedWidget.dart';
+
+void main() {
+  runApp(MyInheritedWidget(appData: MyInheritedData(isFavorite: false), child: const Fooderlich()));
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
+    return MaterialApp(title: 'Fooderlich', theme: theme, home: const Home());
+  }
+}
